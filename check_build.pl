5ac8d30ab33db18e00dd5e6a351632e5c80e2a17
EA3CV <67219942+EA3CV@users.noreply.github.com>
2023-03-02 18:00:26 +0100

    Update

--- check_build.pl
+++ check_build.pl
@@ -11,68 +11,80 @@
 # Include the following line in the crontab:
 # 0 4 * * 1,2,3,4,5 run_cmd("check_build <Y/N>")
 #
+# You need the following package:
+# apt install libfile-copy-recursive-perl  or
+# cpan install File::Copy::Recursive
+#
 # Kin EA3CV, ea3cv@cronux.net
 #
-# 20230209 v1.7
+# 20230302 v1.8
 #
 
+use 5.10.1;
 use DXDebug;
+use File::Copy::Recursive;
 use strict;
 use warnings;
 
 my $self = shift;
 my $bckup = shift;
 
-return 1 unless $self->{priv} >= 9;
+return (1) unless $self->priv >= 9;
+
+my @state = `git remote show origin`;
+system('cd /spider');
 
 my $res;
 my @out;
 
-# Change the working directory to /spider
-chdir '/spider' or die "Failed to change directory: $!";
+if ((/mojo/i ~~ @state) && (/mojo pushes to mojo \(local out of date\)|mojo   publica a mojo   \(desactualizado local\)/i ~~ @state)) {
+        $res = "There is a new build";
+        dbg('DXCron::spawn: $res') if isdbg('cron');
+        push @out, $res;
+        my $load = "*$main::mycall*   🆕  *UPDATE* New build";
+        is_tg($load);
 
-my $remote_status = `git remote show origin`;
-my $has_new_build = $remote_status =~ /mojo/i && $remote_status =~ /mojo pushes to mojo \(local out of date\)|mojo publica a mojo \(desactualizado local\)/i;
+        backup() if $bckup =~ /Y/i;
 
-if ($has_new_build) {
-    $res = "There is a new build";
-#   dbg('DXCron::spawn: $res') if isdbg('cron');
-    push @out, $res;
+        system('git reset --hard');
+        system('git pull');
+        DXCron::run_cmd('shut');
 
-    if ($bckup =~ /Y/i) {
-        my $backup_dir = '/home/sysop/spider.backup';
-        unless (-d $backup_dir) {
-            mkdir $backup_dir or die "Failed to create backup directory: $!";
+} elsif ((/mojo/i ~~ @state) && (/mojo   pushes to mojo   \(up to date\)|mojo   publica a mojo   \(actualizado\)/i ~~ @state)) {
+        $res = "There is no new build";
+        push @out, $res;
+        dbg('DXCron::spawn: $res') if isdbg('cron');
+}
+
+
+sub backup
+{
+        my $from_dir = "/home/sysop/spider";
+        my $to_dir = "/home/sysop/spider.backup";
+
+        if ( !-d $to_dir ) {
+                system('mkdir', $to_dir);
         }
 
-        my $load = "*$self->{mycall}*   💾  *Backup Starts*";
+        my $load = "*$main::mycall*   💾  *Backup Starts*";
         is_tg($load);
 
-        system('rsync -a --delete /home/sysop/spider/ /home/sysop/spider.backup/') == 0 or die "Failed to backup directory: $!";
+        File::Copy::Recursive::rcopy_glob($from_dir, $to_dir);
 
-        $load = "*$self->{mycall}*   🆗  *Backup Completed*";
+        $load = "*$main::mycall*   🆗  *Backup Completed*";
         is_tg($load);
-    }
 
-    # Reset and update the Git repository
-    system('git reset --hard origin/mojo') == 0 or die "Failed to reset Git repository: $!";
-    system('git pull') == 0 or die "Failed to pull updates from Git repository: $!";
-    DXCron::run_cmd('shut');
-} else {
-    $res = "There is no new build";
-    push @out, $res;
-#    dbg('DXCron::spawn: $res') if isdbg('cron');
 }
 
-sub is_tg 
+sub is_tg
 {
-    my $msg = shift;
+        my $msg = shift;
 
-    if (defined &Local::telegram) {
-        my $r;
-        eval { $r = Local::telegram($msg); };
-        return if $r;
-    }
+        if (defined &Local::telegram) {
+                my $r;
+                eval { $r = Local::telegram($msg); };
+                return if $r;
+        }
 }
 
-return (1, @out);
+return (1, @out)
