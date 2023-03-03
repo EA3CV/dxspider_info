261cedcc0648d1a466bac4ecc0b7abe897197b71
EA3CV <67219942+EA3CV@users.noreply.github.com>
2023-03-03 18:59:13 +0100

    Update check_build.pl

--- check_build.pl
+++ check_build.pl
@@ -13,11 +13,10 @@
 #
 # Kin EA3CV, ea3cv@cronux.net
 #
-# 20230303 v1.9
+# 20230209 v1.7
 #
 
 use DXDebug;
-#use DateTime;
 use strict;
 use warnings;
 
@@ -30,48 +29,42 @@ my $res;
 my @out;
 
 # Change the working directory to /spider
-chdir $main::root;
+chdir '/spider' or die "Failed to change directory: $!";
 
 my $remote_status = `git remote show origin`;
 my $has_new_build = $remote_status =~ /mojo/i && $remote_status =~ /mojo   pushes to mojo   \(up to date|mojo   publica a mojo   \(desactualizado local/i;
 
 if ($has_new_build) {
-		$res = "There is a new build";
-		dbg('DXCron::spawn: $res') if isdbg('cron');
-		push @out, $res;
-		if ($bckup =~ /Y/i) {
-			#my $dt = DateTime->now();
-			#my $date = $dt->strftime('%Y%m%d.%H%M%S');
-			my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
-			$year += 1900;
-			$mon++;
-			my $date = sprintf('%04d%02d%02d.%02d%02d%02d', $year, $mon, $mday, $hour, $min, $sec);
-			my $backup_dir = "$main::root/../spider.backup";
-			unless (-d $backup_dir) {
-            mkdir $backup_dir;
-			}
-		
-			my $load = "*$self->{mycall}*   💾  *Backup Starts*";
-			is_tg($load);
+    $res = "There is a new build";
+#   dbg('DXCron::spawn: $res') if isdbg('cron');
+    push @out, $res;
 
-			my $exclusions = "$main::root/local_data/debug,$main::root/local_data/log,$main::root/local_data/spots,$main::root/local_data/wwv,$main::root/local_data/wcy";
-			system("rsync -zavh --exclude={$exclusions} $main::root/ $backup_dir/$date.backup.z");
+    if ($bckup =~ /Y/i) {
+        my $backup_dir = '/home/sysop/spider.backup';
+        unless (-d $backup_dir) {
+            mkdir $backup_dir or die "Failed to create backup directory: $!";
+        }
 
-			$load = "*$self->{mycall}*   🆗  *Backup Completed*";
-			is_tg($load);
-		}
+        my $load = "*$self->{mycall}*   💾  *Backup Starts*";
+        is_tg($load);
 
-		# Reset and update the Git repository
-		system('git reset --hard origin/mojo') == 0 or die push @out,"Failed to reset Git repository: $!";
-		system('git pull') == 0 or die push @out,"Failed to pull updates from Git repository: $!";
-		DXCron::run_cmd('shut');
-		} else {
-		$res = "There is no new build";
-		push @out, $res;
-		dbg('DXCron::spawn: $res') if isdbg('cron');
+        system('rsync -a --delete /home/sysop/spider/ /home/sysop/spider.backup/') == 0 or die "Failed to backup directory: $!";
+
+        $load = "*$self->{mycall}*   🆗  *Backup Completed*";
+        is_tg($load);
+    }
+
+    # Reset and update the Git repository
+    system('git reset --hard origin/mojo') == 0 or die "Failed to reset Git repository: $!";
+    system('git pull') == 0 or die "Failed to pull updates from Git repository: $!";
+    DXCron::run_cmd('shut');
+} else {
+    $res = "There is no new build";
+    push @out, $res;
+#    dbg('DXCron::spawn: $res') if isdbg('cron');
 }
 
-sub is_tg
+sub is_tg 
 {
     my $msg = shift;
 
