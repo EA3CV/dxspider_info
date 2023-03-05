b4b8cd0242dbb3f2f383c23262864dbc86d3d531
EA3CV <67219942+EA3CV@users.noreply.github.com>
2023-03-05 19:56:10 +0100

    Add compress backup

--- check_build.pl
+++ check_build.pl
@@ -13,7 +13,7 @@
 #
 # Kin EA3CV, ea3cv@cronux.net
 #
-# 20230305 v1.11
+# 20230304 v1.12
 #
 
 use DXDebug;
@@ -32,62 +32,69 @@ my @out;
 # Change the working directory to /spider
 chdir "$main::root";
 
-my $remote_status = `git remote show origin`;
-my $has_new_build = $remote_status =~ /mojo/i && $remote_status =~ /mojo   \(local out of date\)|publica a mojo   \(desactualizado local\)/i;
+#my $remote_status = `git remote show origin`;
+#my $has_new_build = $remote_status =~ /mojo/i && $remote_status =~ /mojo   \(local out of date\)|publica a mojo   \(desactualizado local\)/i;
 
-if ($has_new_build) {
-    $res = "There is a new build";
-    dbg('DXCron::spawn: $res') if isdbg('cron');
-    push @out, $res;
+my $local_repo = `git rev-parse \@`;
+my $remote_repo = `git rev-parse \@{u}`;
 
-    if ($bckup =~ /Y/i) {
-#	my $dt = DateTime->now();
-#	my $date = $dt->strftime('%Y%m%d.%H%M%S');
-	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
-	$year += 1900;
-	$mon++;
-	my $date = sprintf('%04d%02d%02d.%02d%02d%02d', $year, $mon, $mday, $hour, $min, $sec);
-        my $backup_dir = "../spider.backup";
-        unless (-d $backup_dir) {
-            mkdir $backup_dir;
-        }
+if ($local_repo ne $remote_repo) {
+	$res = "There is a new build";
+	dbg('DXCron::spawn: $res') if isdbg('cron');
+	push @out, $res;
 
-        my $load = "*$self->{mycall}*   💾  *Backup Starts*";
-        is_tg($load);
+    	if ($bckup =~ /Y/i) {
+		# my $dt = DateTime->now();
+		# my $date = $dt->strftime('%Y%m%d.%H%M%S');
+		my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+		$year += 1900;
+		$mon++;
+		my $date = sprintf('%04d%02d%02d.%02d%02d%02d', $year, $mon, $mday, $hour, $min, $sec);
+        	my $backup_dir = "../spider.backup";
+	        unless (-d $backup_dir) {
+        		mkdir $backup_dir;
+		}
 
-	my @exclude = qw(
-        	--exclude=local_data/debug
-	        --exclude=local_data/log
-        	--exclude=local_data/spots
-	        --exclude=local_data/wwv
-	        --exclude=local_data/wcy
-	);
+        	my $load = "*$self->{mycall}*   💾  *Backup Starts*";
+	        is_tg($load);
 
-	system("rsync", "-azvh", @exclude, '.', "../spider.backup/$date");
+		my @exclude = qw(
+        		--exclude=local_data/debug
+		        --exclude=local_data/log
+        		--exclude=local_data/spots
+	        	--exclude=local_data/wwv
+		        --exclude=local_data/wcy
+		);
 
-        $load = "*$self->{mycall}*   🆗  *Backup Completed*";
-        is_tg($load);
-    }
+		system("rsync", "-avh", @exclude, '.', "../spider.backup/$date");
+		chdir "../spider.backup/";
+		system("tar", "-czvf", "spider.$date.tar.gz", "--remove-files", "$date/");
 
-    # Reset and update the Git repository
-    system('git reset --hard origin/mojo') == 0 or die push @out,"Failed to reset Git repository: $!";
-    system('git pull') == 0 or die push @out,"Failed to pull updates from Git repository: $!";
-    DXCron::run_cmd('shut');
-} else {
-    $res = "There is no new build";
-    push @out, $res;
-    dbg('DXCron::spawn: $res') if isdbg('cron');
+        	$load = "*$self->{mycall}*   🆗  *Backup Completed*";
+	        is_tg($load);
+	}
+
+	chdir "$main::root";
+	# Reset and update the Git repository
+	system('git reset --hard origin/mojo') == 0 or die push @out,"Failed to reset Git repository: $!";
+	system('git pull') == 0 or die push @out,"Failed to pull updates from Git repository: $!";
+	DXCron::run_cmd('shut');
+
+} elsif ($local_repo eq $remote_repo) {
+	$res = "There is no new build";
+	push @out, $res;
+	dbg('DXCron::spawn: $res') if isdbg('cron');
 }
 
 sub is_tg
 {
-    my $msg = shift;
+	my $msg = shift;
 
-    if (defined &Local::telegram) {
-        my $r;
-        eval { $r = Local::telegram($msg); };
-        return if $r;
-    }
+	if (defined &Local::telegram) {
+        	my $r;
+        	eval { $r = Local::telegram($msg); };
+        	return if $r;
+    	}
 }
 
 return (1, @out);
