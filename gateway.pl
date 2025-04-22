aafa201dac0f0eda1e4067465e7f00431dce7652
EA3CV <67219942+EA3CV@users.noreply.github.com>
2025-04-22 13:39:36 +0200

    Update gateway.pl
    IPC usage is improved. Resource usage is reduced.

--- gateway.pl
+++ gateway.pl
@@ -48,12 +48,12 @@
 #
 #  Config:
 #    $mode            = 'mqtt';            # Select input method: 'mqtt' or 'fifo'
-#    $fifo_path       = "/tmp/web_conn_fifo";
+#    $fifo_path       = "./web_conn_fifo";
 #    $timeout         = 300;               # Disconnection timeout for users (seconds)
-#    $interval_pc92c  = 450;               # Interval to send PC92^C summary (seconds)
+#    $interval_pc92c  = 450;               # Interval to send PC92C summary (seconds)
 #
 #  Author  : Kin EA3CV (ea3cv@cronux.net)
-#  Version : 20250420 v0.9
+#  Version : 20250422 v1.0
 #
 #  License : This software is released under the GNU General Public License v3.0 (GPLv3)
 #
@@ -67,8 +67,9 @@ use POSIX qw(strftime);
 use JSON;
 use Net::MQTT::Simple;
 use Fcntl qw(O_RDONLY O_NONBLOCK :flock);
-use IPC::Shareable;
 use LWP::UserAgent;
+use Fcntl ':flock';
+use IPC::Shareable;
 
 # General Configuration
 my @nodes = (
@@ -77,11 +78,17 @@ my @nodes = (
 );
 my $mycall    = 'NODE-9';
 my $password  = 'xxxxxxx';
-my $version   = 'kin_node:0.1';
-my $ipv4      = get_public_ip();  # Get the public IP
+my $version   = 'kin_node:0.2';
+my $ipv4      = get_public_ip();
 
 my $mode       = 'mqtt';  # Options: 'mqtt', 'fifo'
-my $fifo_path  = "/tmp/web_conn_fifo";
+# mqtt
+my $mqtt_host  = '192.168.1.121:1883';
+my $mqtt_topic = 'api/heartbeat/socio';
+my $buffer_file = "./connected_buffer.json";
+# fifo
+my $fifo_path  = "./web_conn_fifo";
+
 my $timeout    = 300;
 my $interval_pc92c = 7200;
 my $interval_pc92k = 3600;
@@ -90,19 +97,15 @@ my $interval_pc92k = 3600;
 my (%counter_uses, $last_day, $last_pc92c, $last_pc92k);
 
 my $pc22_seen;
-tie $pc22_seen, 'IPC::Shareable', 'pc22_flag', { create => 1, mode => 0666 };
+my $pc22_key = 'pc22_seen_key';
+tie $pc22_seen, 'IPC::Shareable', $pc22_key, { create => 1, mode => 0666 };
 $pc22_seen = 0;
-
-my %conectados;
-tie %conectados, 'IPC::Shareable', '/tmp/conectados_shm', { create => 1, mode => 0666 };
-%conectados = ();
 
 %counter_uses = ();
 $last_day     = (gmtime())[3];
 $last_pc92c   = time();
 $last_pc92k   = time();
 
-# Main Loop
 while (1) {
     foreach my $node (@nodes) {
         my $sock = connect_node($node->{host}, $node->{port});
@@ -130,15 +133,50 @@ while (1) {
     }
 }
 
+sub load_buffer {
+    open my $fh, "<", $buffer_file or return {};
+    flock($fh, LOCK_SH);
+    my $json = do { local $/; <$fh> };
+    close $fh;
+    return $json ? decode_json($json) : {};
+}
+
+sub save_buffer {
+    my ($data) = @_;
+    open my $fh, ">", $buffer_file or die "Cannot write buffer: $!";
+    flock($fh, LOCK_EX);
+    print $fh encode_json($data);
+    close $fh;
+}
+
+sub update_buffer {
+    my ($call, $ip) = @_;
+    my $now = time;
+    my $data = load_buffer();
+    $data->{$call}{ip}   = $ip;
+    $data->{$call}{last} = $now;
+    $data->{$call}{start} //= $now;
+    save_buffer($data);
+}
+
+sub delete_buffer_entry {
+    my ($call) = @_;
+    my $data = load_buffer();
+    delete $data->{$call};
+    save_buffer($data);
+}
+
+sub get_all_conectados {
+    return load_buffer();
+}
+
 sub generate_counter {
     my $now = time;
     my $day = (gmtime($now))[3];
-
     if ($day != $last_day) {
         %counter_uses = ();
         $last_day = $day;
     }
-
     my $base_counter = (gmtime($now))[2]*3600 + (gmtime($now))[1]*60 + (gmtime($now))[0];
     $counter_uses{$base_counter}++;
     my $suffix = sprintf(".%02d", $counter_uses{$base_counter});
@@ -148,38 +186,27 @@ sub generate_counter {
 sub tx_pc92 {
     my ($type, $call, $ip, $sock) = @_;
     return unless ($type eq 'C' || $pc22_seen);
-
     my $counter = generate_counter();
     my $pc = $type eq 'A'
         ? "PC92^$mycall^$counter^A^^1$call:$ip^H99^"
         : "PC92^$mycall^$counter^D^^1$call^H99^";
-
-    print $sock "$pc\n";
+    print $sock "$pc";
     log_msg('TX', $pc);
 }
 
 sub connect_node {
     my ($host, $port) = @_;
-    my $sock = IO::Socket::INET->new(
-        PeerHost => $host,
-        PeerPort => $port,
-        Proto    => 'tcp',
-        Timeout  => 10
-    );
-
+    my $sock = IO::Socket::INET->new(PeerHost => $host, PeerPort => $port, Proto => 'tcp', Timeout => 10);
     unless ($sock) {
         log_msg('**', "Could not connect to $host:$port - $!");
         return;
     }
-
     $sock->autoflush(1);
     log_msg('**', "Connected to $host:$port");
-
     print $sock "$mycall\n";
     sleep 1;
     print $sock "$password\n" if $password;
     log_msg('**', "Login sent as $mycall");
-
     return $sock;
 }
 
@@ -187,159 +214,126 @@ sub handle_node {
     my ($sock) = @_;
     my $select = IO::Select->new($sock);
     my ($state, $remote_node) = ('login', '');
-
     while (1) {
-        my $now = time();
-
-        if ($now - $last_pc92c >= $interval_pc92c && keys %conectados) {
-            send_pc92c($sock);
-            $last_pc92c = $now;
-        }
-
-        if ($now - $last_pc92k >= $interval_pc92k) {
-            send_pc92k($sock);
-            $last_pc92k = $now;
-        }
-
-        if ($select->can_read(0.1)) {
+        if ($select->can_read(0.5)) {
             my $line = <$sock>;
             last unless defined $line;
             chomp $line;
-
+            my $now = time();
             if ($line =~ /PC(18|92|20|22|51|11|61)\^/) {
                 my $pc = $1;
-
-                # Ignorar PC92, PC11 y PC61 después de recibir PC22
                 if (($pc == 92 || $pc == 11 || $pc == 61) && $pc22_seen) {
                     next;
                 }
-
                 log_msg('RX', $line);
-
                 if ($state eq 'login' && $pc == 18) {
                     $state = 'pc92_handshake';
                     send_pc92a_k($sock);
                     next;
                 }
-
                 if ($state eq 'pc92_handshake' && $line =~ /^D\s+\Q$mycall\E\s+PC92\^/ && $line =~ /\^K\^/) {
                     $state = 'active';
                 }
-
                 if ($pc == 22) {
                     $pc22_seen = 1;
+                    #log_msg('**', 'PC22 received: pc22_seen = 1');
                 }
-
                 if ($pc == 92 && !$remote_node && $line =~ /^PC92\^([^\^]+)\^\d+\^A\^\^/) {
                     $remote_node = $1;
                 }
-
                 if ($pc == 51 && $line =~ /^PC51\^$mycall\^([^\^]+)\^1\^/) {
                     my $dest = $1;
                     my $reply = "PC51^$dest^$mycall^0^";
-                    print $sock "$reply\n";
+                    print $sock "$reply";
                     log_msg('TX', $reply);
                 }
             }
         }
+        my $now = time();
+        if ($now - $last_pc92c >= $interval_pc92c && scalar keys %{ get_all_conectados() }) {
+            send_pc92c($sock);
+            $last_pc92c = $now;
+        }
+        if ($now - $last_pc92k >= $interval_pc92k) {
+            send_pc92k($sock);
+            $last_pc92k = $now;
+        }
     }
-
     die "Connection lost";
 }
 
 sub send_pc92a_k {
     my ($sock) = @_;
     my $epoch = time();
-
     my ($sec, $min, $hour) = (gmtime($epoch))[0..2];
     my $base_counter = $hour * 3600 + $min * 60 + $sec;
     my $ts_int = $base_counter - ($base_counter % 60);
     my $ts_flt = sprintf("%.2f", $base_counter);
-
-    print $sock "PC92^$mycall^$ts_int^A^^5$mycall:$ipv4^H99^\n"; log_msg('TX', 'Enviado PC92 A');
-    print $sock "PC92^$mycall^$ts_flt^K^5$mycall:9000:1^0^0^$ipv4^$version^H99^\n"; log_msg('TX', 'Enviado PC92 K');
-    print $sock "PC20^\n"; log_msg('TX', 'Enviado PC20');
-
-    $pc22_seen = 1;
+    print $sock "PC92^$mycall^$ts_int^A^^5$mycall:$ipv4^H99^\n"; log_msg('TX', 'Sent PC92 A');
+    print $sock "PC92^$mycall^$ts_flt^K^5$mycall:9000:1^0^0^$ipv4^$version^H99^\n"; log_msg('TX', 'Sent PC92 K');
+    print $sock "PC20^\n"; log_msg('TX', 'Sent PC20');
 }
 
 sub send_pc92c {
     my ($sock) = @_;
     my $counter = generate_counter();
-
-    my $lock_fh = lock_conectados();
-    my $payload = join('^', map { "1$_:$conectados{$_}{ip}" } keys %conectados);
+    my $data = get_all_conectados();
+    my $payload = join('^', map { "1$_:$data->{$_}{ip}" } keys %$data);
     my $pc92c = "PC92^$mycall^$counter^C^5$mycall^$payload^H99^";
-
-    print $sock "$pc92c\n";
-    unlock_conectados($lock_fh);
-    log_msg('TX', "Enviado PC92C: $pc92c");
+    print $sock "$pc92c";
+    log_msg('TX', "Sent PC92C: $pc92c");
 }
 
 sub send_pc92k {
     my ($sock) = @_;
     my $counter = generate_counter();
     my $line = "PC92^$mycall^$counter^K^5$mycall:5457:1^0^0^$ipv4^$version^H99^";
-    print $sock "$line\n";
-    log_msg('TX', "Enviado PC92K: $line");
-}
-
-sub lock_conectados {
-    open my $fh, '>', '/tmp/conectados_lock' or die "Could not open lock file: $!";
-    flock($fh, LOCK_EX) or die "Could not lock file: $!";
-    return $fh;
-}
-
-sub unlock_conectados {
-    my $fh = shift;
-    flock($fh, LOCK_UN) or die "Could not unlock file: $!";
-    close $fh;
+    print $sock "$line";
+    log_msg('TX', "Sent PC92K: $line");
 }
 
 sub run_mqtt_loop {
     my ($sock) = @_;
     my $mqtt;
-
+    my $last_check = time();
     while (1) {
         eval {
-            $mqtt = Net::MQTT::Simple->new('192.168.1.121:1883');
-            $mqtt->subscribe('api/heartbeat/socio' => sub {
+            $mqtt = Net::MQTT::Simple->new($mqtt_host);
+            $mqtt->subscribe($mqtt_topic => sub {
                 my ($topic, $message) = @_;
                 my $data = eval { decode_json($message) };
                 return unless $data and $data->{call};
-
                 my $call = uc($data->{call});
-                my $now  = time;
-                my @ips = map { s/^\s+|\s+\$//gr } split /,/, $data->{ip};
-
+                my @ips = map { s/^\s+|\s+$//gr } split /,/, $data->{ip};
                 for my $i (0..$#ips) {
                     my $ip = $ips[$i];
                     my $full_call = $i == 0 ? $call : "$call-" . (19 + $i);
-
-                    my $already = exists $conectados{$full_call};
-                    $conectados{$full_call}{ip}   = $ip;
-                    $conectados{$full_call}{last} = $now;
-
-                    unless ($already) {
-                        $conectados{$full_call}{start} = $now;
-                        tx_pc92('A', $full_call, $ip, $sock);
-                    }
+                    my $buffer = get_all_conectados();
+                    my $already = exists $buffer->{$full_call};
+                    update_buffer($full_call, $ip);
+                    tx_pc92('A', $full_call, $ip, $sock) unless $already;
                 }
             });
-
             while (1) {
                 $mqtt->tick();
-                my $now = time;
-                for my $call (keys %conectados) {
-                    if ($now - $conectados{$call}{last} > $timeout) {
-                        tx_pc92('D', $call, '', $sock);
-                        delete $conectados{$call};
+                my $now = time();
+                if ($now - $last_check >= 10) {
+                    my $data = get_all_conectados();
+                    my @desconectar;
+                    for my $call (keys %$data) {
+                        if ($now - $data->{$call}{last} > $timeout) {
+                            push @desconectar, $call;
+                        }
                     }
+                    for my $call (@desconectar) {
+                        tx_pc92('D', $call, '', $sock);
+                        delete_buffer_entry($call);
+                    }
+                    $last_check = $now;
                 }
-                sleep 1;
+                sleep 0.5;
             }
         };
-
         log_msg('**', "MQTT error: $@. Retrying in 10s...");
         sleep 10;
     }
@@ -347,61 +341,44 @@ sub run_mqtt_loop {
 
 sub run_fifo_loop {
     my ($sock) = @_;
-
     while (1) {
         unless (-p $fifo_path) {
             unlink $fifo_path;
             system("mkfifo", $fifo_path);
-            log_msg('**', "FIFO $fifo_path created");
+            #log_msg('**', "FIFO $fifo_path created");
         }
-
         sysopen(my $fh, $fifo_path, O_RDONLY | O_NONBLOCK) or do {
-            log_msg('**', "Failed to open FIFO $fifo_path: $!");
+            #log_msg('**', "Failed to open FIFO $fifo_path: $!");
             sleep 5;
             next;
         };
-
         log_msg('**', "FIFO $fifo_path opened for reading");
         my $selector = IO::Select->new($fh);
-
         while (1) {
             unless (-p $fifo_path) {
                 log_msg('**', "FIFO $fifo_path was removed, restarting...");
                 last;
             }
-
             if ($selector->can_read(0.1)) {
                 my $line = <$fh>;
-
                 unless (defined $line) {
                     log_msg('**', "FIFO closed (writer end disappeared), reopening...");
                     last;
                 }
-
                 chomp $line;
                 next if $line =~ /^\s*$/;
-
                 if ($line =~ /^CONN,(\S+),(\S+)/) {
                     my ($call, $ip) = (uc($1), $2);
-                    my $now = time;
-
-                    my $already = exists $conectados{$call};
-                    $conectados{$call}{ip}   = $ip;
-                    $conectados{$call}{last} = $now;
-
-                    unless ($already) {
-                        $conectados{$call}{start} = $now;
-                        tx_pc92('A', $call, $ip, $sock);
-                    }
-
+                    my $already = exists get_all_conectados()->{$call};
+                    update_buffer($call, $ip);
+                    tx_pc92('A', $call, $ip, $sock) unless $already;
                 } elsif ($line =~ /^DESC,(\S+)/) {
                     my $call = uc($1);
                     tx_pc92('D', $call, '', $sock);
-                    delete $conectados{$call};
+                    delete_buffer_entry($call);
                 }
             }
         }
-
         close $fh;
         sleep 1;
     }
